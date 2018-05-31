3a81e6501027d6971fcfb9a66b0f4ec549d881a8
Mario Garcia <mario.ggar@gmail.com>
2018-05-31 08:44:44 +0200

    Fix unused import

--- gql-core/src/main/groovy/gql/DSL.groovy
+++ gql-core/src/main/groovy/gql/DSL.groovy
@@ -1,36 +1,36 @@
 package gql
 
+import java.util.concurrent.CompletableFuture
+import groovy.transform.stc.ClosureParams
+import groovy.transform.stc.SimpleType
+
 import gql.dsl.EnumTypeBuilder
+import gql.dsl.ExecutionBuilder
+import gql.dsl.GraphQLErrorBuilder
 import gql.dsl.InputTypeBuilder
 import gql.dsl.InterfaceBuilder
+import gql.dsl.ObjectTypeBuilder
 import gql.dsl.QueryBuilder
 import gql.dsl.ScalarTypeBuilder
 import gql.dsl.SchemaBuilder
-import gql.dsl.ObjectTypeBuilder
 import gql.dsl.SchemaMergerBuilder
-import gql.dsl.ExecutionBuilder
-import gql.dsl.GraphQLErrorBuilder
-import graphql.GraphQL
-import graphql.GraphQLError
 import graphql.ExecutionInput
 import graphql.ExecutionResult
-import graphql.execution.ExecutorServiceExecutionStrategy
+import graphql.GraphQL
+import graphql.GraphQLError
+import graphql.execution.ExecutionPath
+import graphql.execution.instrumentation.Instrumentation
+import graphql.execution.instrumentation.parameters.InstrumentationFieldFetchParameters
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
 import graphql.schema.GraphQLEnumType
 import graphql.schema.GraphQLFieldDefinition
 import graphql.schema.GraphQLInputObjectType
 import graphql.schema.GraphQLInterfaceType
+import graphql.schema.GraphQLObjectType
 import graphql.schema.GraphQLScalarType
 import graphql.schema.GraphQLSchema
-import graphql.schema.GraphQLObjectType
 import graphql.schema.TypeResolver
-import graphql.execution.ExecutionPath
-import graphql.execution.instrumentation.Instrumentation
-import graphql.execution.instrumentation.parameters.InstrumentationFieldFetchParameters
-import groovy.transform.stc.ClosureParams
-import groovy.transform.stc.SimpleType
-import java.util.concurrent.CompletableFuture
 
 /**
  * Functions in this class ease the creation of different GraphQL
