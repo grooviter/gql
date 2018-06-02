63ec97cc9000e6479f072d50ce6c9c0ccfa7425f
Mario Garcia <mario.ggar@gmail.com>
2018-06-02 22:11:57 +0200

    Fix instrumentation dependency from mandatory -> optional

--- gql-ratpack/src/main/java/gql/ratpack/GraphQLProvider.java
+++ gql-ratpack/src/main/java/gql/ratpack/GraphQLProvider.java
@@ -1,28 +1,53 @@
 package gql.ratpack;
 
-import javax.inject.Inject;
 import javax.inject.Provider;
+import java.util.Optional;
 
+import com.google.inject.Inject;
 import graphql.GraphQL;
 import graphql.execution.instrumentation.Instrumentation;
 import graphql.schema.GraphQLSchema;
 
+/**
+ * Creates an instance of {@link GraphQL} that can be reused among all
+ * incoming requests.
+ *
+ * @since 0.3.2
+ */
 public class GraphQLProvider implements Provider<GraphQL> {
 
-  private final GraphQLSchema graphQLSchema;
-  private final Instrumentation instrumentation;
+  private GraphQLSchema graphQLSchema;
+  private Instrumentation instrumentation;
 
+  /**
+   * Sets the {@link GraphQLSchema} instance. This is a hard
+   * dependency
+   *
+   * @since 0.3.2
+   */
   @Inject
-  public GraphQLProvider(GraphQLSchema schema, Instrumentation instrumentation) {
+  public void setGraphQLSchema(GraphQLSchema schema) {
     this.graphQLSchema = schema;
+  }
+
+  /**
+   * Sets the {@link Instrumentation} instance used. This is a soft
+   * dependency
+   *
+   * @since 0.3.2
+   */
+  @Inject(optional = true)
+  public void setInstrumentation(Instrumentation instrumentation) {
     this.instrumentation = instrumentation;
   }
 
   @Override
   public GraphQL get() {
-    return GraphQL
-      .newGraphQL(graphQLSchema)
-      .instrumentation(instrumentation)
-      .build();
+    final GraphQL.Builder builder = GraphQL.newGraphQL(graphQLSchema);
+
+    return Optional
+      .ofNullable(instrumentation)
+      .map(ins -> builder.instrumentation(ins).build())
+      .orElse(builder.build());
   }
 }
